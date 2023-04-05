689355ea419a52649c853faa3faf7bde7874750a
Alessandro C <s303508@studenti.polito.it>
2023-04-05 13:14:20 +0000

    Added some comments and updated Power point

--- src/kern/vm/pt.c
+++ src/kern/vm/pt.c
@@ -2,6 +2,7 @@
 
 static uint8_t index;
 
+/*Allocation of the data structure for the page table*/
 paddr_t * pt_create(size_t nentries)
 {
     paddr_t * new_pt = kmalloc(sizeof(paddr_t) * nentries);
@@ -12,7 +13,7 @@ paddr_t * pt_create(size_t nentries)
 
     return new_pt;
 }
-
+/**De-allocation of data strcuture for the page table*/
 void pt_destroy(paddr_t * pt_dest)
 {
     kfree(pt_dest);
@@ -20,26 +21,25 @@ void pt_destroy(paddr_t * pt_dest)
     return;
 }
 
-/*Questa funzione,tramite index seleziona una vittima tra le entry della page table da rimpiazzare 
-secondo un algoritmo di replacement*/
+/*This function uses index to select a victim among entries of page table to replace*/
 paddr_t get_victim_frame(paddr_t *pt,uint32_t *entry_valid)
 {
         return pt[entry_valid[index]];
 }
-
+/*This function allows to extract from entry valid the victim entry of the page table*/
 uint32_t get_victim_pt_index(uint32_t *entry_valid)
 {
         return entry_valid[index];
 }
-
+/*This function allows to extract the page number of a valid page whose virtualaddr belong to*/
 vaddr_t get_page_number(vaddr_t virtualaddr,uint32_t *entry_valid)
 {
         return ((entry_valid[index]*PAGE_SIZE) + virtualaddr) & PAGE_FRAME;
 }
 
 
-/*Questa funzione,aggiorna una entry della page table e contemporaneamente un elemento del vettore entry valid:
-- entry_valid è il vettore tiene conto delle sole pagine caricate in memoria (valide) in modo da rispettare il DEMAND PAGING.*/
+/*This function updates an entry of PAGE TABLE and in the meanwhile an element of ENTRY VALID:
+- entry_valid is the vector used to keep track of only the pages loaded in memory(VALID) in order to implement DEMAND PAGING.*/
 void pt_update(paddr_t *pt,uint32_t *entry_valid,paddr_t new_frame,uint8_t nvalidentries,uint32_t new_pt_index)
 {
     
